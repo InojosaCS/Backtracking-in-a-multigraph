c1ee89ea5c2f629ebd3f80ca37e73818f92ff609
Cristian Inojosa Siso <50759877+InojosaCS@users.noreply.github.com>
2020-03-29 14:59:08 -0400

    pa ve

--- PlanearTrasbordos.java
+++ PlanearTrasbordos.java
@@ -43,12 +43,12 @@ public class PlanearTrasbordos {
 			}
 			Lector.close(); 
 			
-			
-			MapeoEstaciones(args[0], args[1]);
+			LeerEstaciones(args[0]);
+			//MapeoEstaciones(args[0], args[1]);
 			grafo.cargarGrafo(args[0]);
 			CambiarTexto b = new CambiarTexto();
 			b.RecuperarArchivo(args[0], lineasDeMetro);
-
+			
 			// Busca los vertices iniciales y finales
 			Collection<Vertice> vertices = grafo.vertices();
 			nroEstaciones = vertices.size();
@@ -65,7 +65,7 @@ public class PlanearTrasbordos {
 				if(vertice.getNombre().contentEquals(args[3])) 	
 					verticeFin = grafo.obtenerVertice(vertice.getId());
 			}
-			
+		
 			if(verticeInicio.getNombre().contentEquals("") || verticeFin.getNombre().contentEquals("")) {
 				System.out.println("<pIncio> o <pFin> no pertence al conjunto de estaciones en <mapa>, chequee que esten escritas igual");
 				return;
@@ -87,6 +87,53 @@ public class PlanearTrasbordos {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+	}
+
+	private static boolean LeerEstaciones(String archivo) {
+		try {
+			// Leer las lineasDeMetro y contar cuantas lineas de metro hay
+	        BufferedReader Lector = new BufferedReader(new FileReader(archivo));
+	        String linea = Lector.readLine();
+	        
+	        int n = Integer.parseInt(Lector.readLine());
+            int m = Integer.parseInt(Lector.readLine());
+            
+            for(int i=0; i<n; i++) {
+    	        linea = Lector.readLine();
+            }
+            
+            LinkedList<String> ciudadesa = new LinkedList<String>();
+            
+            for(int i=0; i<m; i++) {
+    	        String[] helper = Lector.readLine().split("\\s+");
+    	        boolean aux = true;
+    	        
+    	        for(String s: ciudadesa) {
+    	        	if(s.contentEquals(helper[2])) {
+    	        		aux = false;
+    	        		break;
+    	        	}
+    	        }
+    	        if(aux) {
+    	        	ciudadesa.add(helper[2]);
+    	        }
+            } 
+            
+        	nroLineas = ciudadesa.size();
+            lineasDeMetro = new String [nroLineas];
+			CambiarTexto f = new CambiarTexto();
+            
+            for(int i=0; i<lineasDeMetro.length; i++) {
+            	lineasDeMetro[i] = ciudadesa.get(i);
+				f.modifyFile(archivo, "\\b"+lineasDeMetro[i]+"\\b", Integer.toString(i));
+            }           
+            
+	        Lector.close();
+	        
+		} catch(Exception e) {
+			return false;
+		}
+		return true;
 	}
 
 
@@ -124,122 +171,51 @@ public class PlanearTrasbordos {
 		return true;
 	}
 	
-	private static String backtracking(GrafoNoDirigido grafo, Vertice inicio, Vertice fin) { 
-		// Declaracion de variables
-		Set<Vertice> adyacentes = grafo.adyacentes(inicio.getId());
-		int current=0;
-		String caminoActual = inicio.getNombre();
-		
-		// Se crea la tabla de visitados
-		int k = 0;
-		int [][] tablaVisitados = new int[nroLineas+2][nroEstaciones];
-		
-		for(Vertice vertice: grafo.vertices()) {
-			tablaVisitados [0][k] = vertice.getId();
-			for(int j=1; j<nroLineas+1; j++) {
-				tablaVisitados[j][k] = 0;
-			}
-			k++;
-		}
-		// Se itera sobre sobre los vecinos del nodo inicial
-		for(Vertice verticeAdj: adyacentes) {
-			System.out.println(verticeAdj.getNombre());
-			// Se busca cuales lines existen entre los nodos y se busca un camino entre ellos
-			for(int i=0; i<nroLineas; i++) {
-				int indiceValido = visitaValida((GrafoNoDirigido) grafo, verticeAdj, i, tablaVisitados);
-				if (grafo.estaArista(inicio, verticeAdj, i) && indiceValido!=-1) {
-					Arista arista = new Arista(inicio, verticeAdj, i, 0);
-					arista = grafo.obtenerArista(inicio, verticeAdj, i);
-					BTRecursivo((GrafoNoDirigido) grafo, arista, current, fin.getNombre(), caminoActual, tablaVisitados);
-					tablaVisitados[i+1][indiceValido] += 1;
-				}
-			}
-		}
-		return caminoCompleto;
-	}
-	
-	static void BTRecursivo(GrafoNoDirigido grafo, Arista arista, int current, String destino, String caminoActual, int [][] tablaVisitados) { 
-		// Declaracion de variables
-		Vertice inicio = arista.getExtremo1();
-		Vertice actual = arista.getExtremo2();
-		int tipo = arista.getTipo();
-		Set<Vertice> adyacentes = grafo.adyacentes(actual.getId());
-		System.out.println(inicio.getNombre()+"->"+actual.getNombre()+" "+tipo);
-		
-		caminoActual += " " + lineasDeMetro[tipo] + " -> " + actual.getNombre() + " " + Integer.toString(actual.getId());
-		
-		// si llega al destino entonces acaba el backtracking y asigna los nuevos valores
-		if( actual.getNombre().contentEquals(destino) ) {
-			minimo = current < minimo ? current : minimo;
-			caminoCompleto = caminoActual;
-			System.out.println(caminoCompleto);
-			System.out.println(minimo);
-			return;
-		}
-		
-		// Si llega a un nodo sin vecinos entonces acaba el backtracking en esa rama
-		for(Vertice verticeAdj: adyacentes) {
-			// Se chequea si exite una arista del mismo tipo de linea y si 
-			// es valido visitarla (esto es, que no haya sido visitado ese nodo por la misma linea)
-			int indiceValido = visitaValida((GrafoNoDirigido) grafo, verticeAdj, tipo, tablaVisitados);
-			if (grafo.estaArista(actual, verticeAdj, tipo) && indiceValido!=-1 && verticeAdj.getNombre() != inicio.getNombre()) {
-				tablaVisitados[tipo+1][indiceValido] += 1;
-				Arista aristaAux = new Arista(actual, verticeAdj, tipo, 0);
-				aristaAux = grafo.obtenerArista(actual, verticeAdj, tipo);
-				BTRecursivo((GrafoNoDirigido) grafo, aristaAux, current, destino, caminoActual, tablaVisitados);
-				//break;
-				}
-		}
-		
-		for(Vertice verticeAdj: adyacentes) {
-			// Se itera sobre todos los adyacentes del nodo actual 
-			for(int i=0; i<nroLineas; i++) {
-				// Se chequea si exite una arista por cualquier tipo de linea y si 
-				// es valido visitarla (esto es, que no haya sido visitado ese nodo por la misma linea)
-				int indiceValido = visitaValida((GrafoNoDirigido) grafo, verticeAdj, i, tablaVisitados);
-				if (grafo.estaArista(actual, verticeAdj, i) && current + 1 < minimo && indiceValido!=-1 && verticeAdj.getNombre() != inicio.getNombre()) {
-					tablaVisitados[i+1][indiceValido] += 1;
-					Arista aristaAux = new Arista(actual, verticeAdj, i, 0);
-					aristaAux = grafo.obtenerArista(actual, verticeAdj, i);
-					BTRecursivo((GrafoNoDirigido) grafo, aristaAux, current+1, destino, caminoActual, tablaVisitados);
-				}
-			}
-		}
-		return;
-	}
-		
-	static int visitaValida(GrafoNoDirigido grafo, Vertice v, int tipo, int [][] tablaVisitados) {
-		int i=0;
-		int ans = -1;
-		boolean helper = false;
-		// Cheque si una vertice ya ha sido visitado por una linea
-		for(Vertice vertice: grafo.vertices()) {
-			if(vertice.getId() == v.getId()) {
-				helper = tablaVisitados[tipo+1][i]<3;
-				break;
-			}
-			i++;
-		}
-		return ans = helper ? i : ans;
-	}
+	private static String backtracking(GrafoNoDirigido grafo, Vertice inicio, Vertice fin) { return "";	}
 
 	private static String backtracking(GrafoDirigido grafo, Vertice inicio, Vertice fin) { 
 		// Declaracion de variables
-		Set<Vertice> sucesores = grafo.sucesores(inicio.getId());
+		Set<Vertice> sucesores = grafo.sucesores(verticeInicio.getId());
 		int current=0;
 		String caminoActual = inicio.getNombre();
-
+		
+		// Heuristica
+		Vertice masCercano = new Vertice (-1, "", -1, -1, -1);
+		Double distanciaMinima = Double.MAX_VALUE;
+		boolean [] visitados = new boolean [nroEstaciones];
+		visitados[hashing.get(verticeInicio.getId())] = true;
+		
+		for(Vertice verticeAdj: sucesores) {
+			if( Math.sqrt(Math.pow((verticeAdj.getX() - verticeFin.getX()),2) + Math.pow((verticeAdj.getY() - verticeFin.getY()),2)) < distanciaMinima){
+				masCercano = verticeAdj;
+				distanciaMinima = Math.sqrt(Math.pow((verticeAdj.getX() - verticeFin.getX()),2) + Math.pow((verticeAdj.getY() - verticeFin.getY()),2));
+			}
+		}
+		/*
+		System.out.println(masCercano.getNombre());
+		for(int i=0; i<nroLineas; i++) {
+			if (grafo.estaArco(inicio, masCercano, i)) {
+				Arco arcoU = new Arco(inicio, masCercano, i, 0);
+				arcoU = grafo.obtenerArco(inicio, masCercano, i);
+				visitados[hashing.get(masCercano.getId())] = true;
+				BTRecursivo((GrafoDirigido) grafo, arcoU, 0, caminoActual, visitados);
+				visitados[hashing.get(masCercano.getId())] = false;
+			}
+		}
+*/
+		//visitados[hashing.get(masCercano.getId())] = true;
+		
 		// Se itera sobre sobre los vecinos del nodo inicial
 		for(Vertice verticeAdj: sucesores) {
 			System.out.println(verticeAdj.getNombre());
 			// Se busca cuales lines existen entre los nodos y se busca un camino entre ellos
 			for(int i=0; i<nroLineas; i++) {
-				if (grafo.estaArco(inicio, verticeAdj, i)) {
+				if (grafo.estaArco(inicio, verticeAdj, i) && !visitados[hashing.get(verticeAdj.getId())]) {
 					Arco arco = new Arco(inicio, verticeAdj, i, 0);
 					arco = grafo.obtenerArco(inicio, verticeAdj, i);
-					boolean [] visitados = new boolean [nroEstaciones];
 					visitados[hashing.get(verticeAdj.getId())] = true;
-					BTRecursivo((GrafoDirigido) grafo, arco, current, caminoActual, visitados);
+					BTRecursivo((GrafoDirigido) grafo, arco, 0, caminoActual, visitados);
+					visitados[hashing.get(verticeAdj.getId())] = false;
 				}
 			}
 		}
@@ -251,11 +227,16 @@ public class PlanearTrasbordos {
 		Vertice inicio = arco.getExtremoInicial();
 		Vertice actual = arco.getExtremoFinal();
 		int tipo = arco.getTipo();
+
+		visitados[hashing.get(actual.getId())] = true;
+		
 		Set<Vertice> sucesores = grafo.sucesores(actual.getId());
 		//System.out.println(inicio.getNombre()+"->"+actual.getNombre()+" "+lineasDeMetro[tipo]);
 		
 		caminoActual += " " + lineasDeMetro[tipo] + " -> " + actual.getNombre() + " " + Integer.toString(actual.getId());
-		if(actual.getId()==verticeInicio.getId()) current = 0;
+		String[] helper = caminoActual.split("\\s+");
+		
+		if(actual.getId()==verticeInicio.getId() || helper.length>200 || minimo==0 ) {	return;}
 		
 		// si llega al destino entonces acaba el backtracking y asigna los nuevos valores
 		if( actual.getId()==verticeFin.getId() && current < minimo) {
@@ -263,8 +244,10 @@ public class PlanearTrasbordos {
 			caminoCompleto = caminoActual;
 			System.out.println(caminoCompleto);
 			System.out.println(minimo);
+			System.out.println(helper.length);
 			return;
 		}
+		
 		
 		// Si llega a un nodo sin vecinos entonces acaba el backtracking en esa rama
 		for(Vertice verticeAdj: sucesores) {
@@ -276,26 +259,58 @@ public class PlanearTrasbordos {
 				arcoAux = grafo.obtenerArco(actual, verticeAdj, tipo);
 				BTRecursivo((GrafoDirigido) grafo, arcoAux, current, caminoActual, visitados);
 				visitados[hashing.get(verticeAdj.getId())] = false;
-				//break;
 				}
 		}
 		
+		Vertice masCercanox = new Vertice (-1, "", -1, -1, -1);
+		Double distanciaMinima = Double.MAX_VALUE;
+		
+		for(Vertice verticeAdj: sucesores) {
+			if( Math.sqrt(Math.pow((verticeAdj.getX() - verticeFin.getX()),2) + Math.pow((verticeAdj.getY() - verticeFin.getY()),2)) < distanciaMinima){
+				masCercanox = verticeAdj;
+				distanciaMinima = Math.sqrt(Math.pow((verticeAdj.getX() - verticeFin.getX()),2) + Math.pow((verticeAdj.getY() - verticeFin.getY()),2));
+			}
+		}
+		
+		//System.out.println(masCercano.getNombre());
+		/*
+		for(int x=0; x<nroLineas;x++) {
+			if (grafo.estaArco(actual, masCercanox, x) && masCercanox.getId() != inicio.getId() && !visitados[hashing.get(masCercanox.getId())]) {
+				Arco arcoA = new Arco(actual, masCercanox, x, 0);
+				arcoA = grafo.obtenerArco(actual, masCercanox, x);
+				visitados[hashing.get(masCercanox.getId())] = true;
+				if(x==tipo)	{
+					BTRecursivo((GrafoDirigido) grafo, arcoA, current, caminoActual, visitados);
+				}
+				else if(current+1<minimo) {
+					BTRecursivo((GrafoDirigido) grafo, arcoA, current+1, caminoActual, visitados);
+					//visitados[hashing.get(masCercanox.getId())] = false;
+				}
+				//visitados[hashing.get(masCercanox.getId())] = false;	
+			}	
+		}	
+		*/
 		for(Vertice verticeAdj: sucesores) {
 			// Se itera sobre todos los sucesores del nodo actual 
 			for(int i=0; i<nroLineas; i++) {
 				// Se chequea si exite una arco por cualquier tipo de linea y si 
 				// es valido visitarla (esto es, que no haya sido visitado ese nodo por la misma linea)
-				
 				if (grafo.estaArco(actual, verticeAdj, i) && current + 1 < minimo  && !visitados[hashing.get(verticeAdj.getId())]) {
 					visitados[hashing.get(verticeAdj.getId())] = true;
 					Arco arcoAux = new Arco(actual, verticeAdj, i, 0);
 					arcoAux = grafo.obtenerArco(actual, verticeAdj, i);
-					current = i==tipo ? current : current+1;
-					BTRecursivo((GrafoDirigido) grafo, arcoAux, current, caminoActual, visitados);
+					if(i==tipo)	{
+					}
+					else if(current+1<minimo) {
+						BTRecursivo((GrafoDirigido) grafo, arcoAux, current+1, caminoActual, visitados);
+						//visitados[hashing.get(masCercanox.getId())] = false;
+					}
 					visitados[hashing.get(verticeAdj.getId())] = false;
 				}
 			}
 		}
+		visitados[hashing.get(actual.getId())] = true;
 		return;
 	}
+	
 }
